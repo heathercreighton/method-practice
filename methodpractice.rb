91d363fa84e09d95fa561615fe4cdfdb96db48cf
Heather Creighton <heather.creighton@gmail.com>
2016-11-22 12:17:59 -0500

    first commit

---
+++ methodpractice.rb
@@ -0,0 +1,151 @@
+# def two_plus_two
+# 	sum = 2+2
+# 	return sum
+# end
+
+# puts two_plus_two	
+
+
+# def add_it_up(num1,num2)
+# 	sum = num1 + num2
+# end
+
+
+# puts add_it_up(6, 12)
+# puts add_it_up(12, 55)
+# puts add_it_up(34, 910)
+
+
+# time = 6
+# space = 33
+# puts add_it_up(time, space)
+
+
+
+# #convert pound to kilograms
+
+# def lbs_to_kilos(weight)
+# 	kilos = weight * 0.446
+# end
+
+# puts lbs_to_kilos(120)	
+
+# #computes length of string
+# def string_length(string)
+# 	length = string.length
+# end
+
+# puts string_length("this is a great day")	
+
+# #reverses a string
+# def reverse(string)
+# 	new_string = string.reverse
+# end	
+
+# puts reverse("This is an awesome day")
+
+# def get_num
+# 	puts "Give me a number"
+# 	input1 = gets.chomp.to_i
+# 	puts "Give me another number"
+# 	input2 = gets.chomp.to_i
+# 		if input1 ==0 || input2 == 0
+# 			puts "Neither numbers can be zero, try again"
+# 			get_num
+# 		else 
+# 			gets_remainder(input1, input2)	
+# 		end
+# end		
+
+
+# def gets_remainder(num1, num2)
+
+# 	remainder = num1%num2
+# 	quot = num1/num2
+# 	puts "The quotient of #{num1} and #{num2} is #{quot} and the remainder is #{remainder}."
+
+# end
+
+
+
+# get_num		
+
+
+
+
+# sum1 = 8 + 4
+# sum2 = 18 + 89
+# sum3 = 32 +77
+
+# prod1 = 8 * 4
+# prod2 = 18* 89
+# prod3 = 32 * 77
+
+
+# puts sum1, sum2, sum3, prod1, prod2, prod3
+
+
+
+
+# def gets_sum(num1,num2)
+# 	puts num1 + num2
+# 	puts num1 * num2
+# end
+
+# gets_sum(8, 4)
+# gets_sum(18, 99)
+# gets_sum(32,77)
+
+
+# visited = []
+
+# puts "Enter all the states you have been to. Type 'done' when finished"
+
+
+# keep_going = true
+
+# while keep_going
+# 	state = gets.chomp.upcase
+
+# 	if state != "DONE"
+# 		visited.push(state)
+
+# 	else
+# 		keep_going = false	
+# 	end	
+
+# end
+
+# puts "These are the states you've visited: "
+# visited.each do |state|
+# 	puts state.capitalize
+# end	
+
+
+
+#####*************************************#
+#Student_shuffle
+
+students = ["Daniele", "Heather", "Anita", "Ben", "Thomas", "Matthew", "Mark", "Jennifer", "Cameron"]
+
+puts students
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
